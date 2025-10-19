231f9ec61a8618cf7f33460a3ab7f207baa06fd1
Tejas Gupta <tgupta39@wisc.edu>
2025-10-19 14:25:28 -0500

    Changing backend/config/middleware.js to explicitly allow cors

--- backend/config/middlewares.js
+++ backend/config/middlewares.js
@@ -2,7 +2,16 @@ module.exports = [
   'strapi::logger',
   'strapi::errors',
   'strapi::security',
-  'strapi::cors',
+  {
+    name: 'strapi::cors',
+    config: {
+      enabled: true,
+      origin: ['http://localhost:3000'], // frontend URL, after deployment change to -> origin: ['https://frontend-domain.com']
+      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+      headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
+      keepHeaderOnError: true,
+    },
+  },
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
