fe6538db5e9296f50c1e89dc673c35ce51c059ed
Kiet Pham <paulpham.kiet@gmail.com>
2025-10-20 09:26:16 -0500

    Some contact us work first, still missing form and map

--- frontend/src/app/contact-us/page.jsx
+++ frontend/src/app/contact-us/page.jsx
@@ -1,3 +1,51 @@
+"use client"
+
+import Link from 'next/link'; 
+import "./styles.css"
+
 export default function ContactUsPage() {
-  return <h1>Contact Us</h1>;
+  return (<div className="contact-us-page">
+
+    <h1>LET'S CONNECT</h1>
+    <div className="extended-form">
+      <div className="info-box">
+        <div>
+        <img src="/Graphic_Arrow_Black.png" style={{ height: "60px" }}></img>
+        <h2>ATLATL <br/> ADVISERS</h2>
+        </div>
+        <div>
+          <p>608-351-4500</p>
+          <p>info@atlatladvisers.com</p>
+        </div>
+        <div>
+          2921 Landmark Place Suite 501 <br /> Madison, WI 53711
+        </div>
+        <button>SCHEDULE A MEETING</button>
+      </div>
+      <div className="contact-form">
+        <div>
+          <div>
+            First Name 
+            <input></input>
+          </div>
+          <div>
+            Last Name
+            <input></input>
+          </div>
+        </div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+
+        <button>SUBMIT</button>
+      </div>
+    </div>
+    
+    {/** The interactive Map */}
+    <div></div>
+
+    <h1>QUESTIONS?</h1>
+    <Link href="/" className="faq">VIEW FAQ</Link>
+  </div>);
 }
