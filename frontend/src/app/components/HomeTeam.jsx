891d9bf0ed7bc8b8e3b46bc18533a643ee737628
Khoa Cao <kqcao@wisc.edu>
2025-10-26 14:21:22 -0500

    feat: :sparkles: adjust on padding and moving to tailwind

--- frontend/src/app/components/HomeTeam.jsx
+++ frontend/src/app/components/HomeTeam.jsx
@@ -1,35 +1,60 @@
 "use client";
 
 import Link from "next/link";
-import "./HomeTeam.css";
 
 export default function HomeTeam() {
   return (
-    <div className="hometeam-board">
-        <div className="hometeam-text">
-            <h1>PEOPLE WITH A PURPOSE</h1>
-            <p>We strive to hire, inspire, and invest in the best talent across our respective disciplines and empower our team to thrive in an environment of respect. Our ability to celebrate different perspectives and foster a culture of inclusion leads to an exceptional client experience.</p>
+    <div className="flex flex-col items-center w-full">
+      <div className="flex flex-col justify-center items-center w-full font-work-sans bg-dark-blue text-white text-center">
+        <h1 className="text-4xl lg:text-5xl xl:text-6xl font-bold font-songer pt-12">
+          PEOPLE WITH A PURPOSE
+        </h1>
+        <p className="text-lg px-[20%] py-12">
+          We strive to hire, inspire, and invest in the best talent across our
+          respective disciplines and empower our team to thrive in an
+          environment of respect. Our ability to celebrate different
+          perspectives and foster a culture of inclusion leads to an exceptional
+          client experience.
+        </p>
+      </div>
+      <div className="w-full flex flex-col lg:flex-row items-center lg:items-start lg:justify-between px-6 lg:px-[15%] gap-12 lg:gap-8 bg-gradient-to-b from-dark-blue from-50% to-white to-50% text-text-light-blue">
+        <div className="flex flex-col items-center font-work-sans text-center lg:w-[30%]">
+          <img
+            src="images/placeholder-vertical.png"
+            className="h-[280px] sm:h-[320px] lg:h-[360px] object-cover mb-4"
+            alt="Ross Fedenia"
+          />
+          <h2 className="font-songer text-lg font-bold">ROSS FEDENIA, CFP</h2>
+          <div>Founder and CEO</div>
         </div>
-        <div className="member-card-list">
-            <div className="member-card">
-                <img src="images/placeholder-vertical.png" className="portrait"></img>
-                <h2>ROSS FEDENIA, CFP</h2>
-                <div>Founder and CEO</div>
-            </div>
-            <div className="member-card">
-                <img src="images/placeholder-vertical.png" className="portrait"></img>
-                <h2>MARK FEDENIA, PH.D</h2>
-                <div>Director of Investments</div>
-            </div>
-            <div className="member-card">
-                <img src="images/placeholder-vertical.png" className="portrait"></img>
-                <h2>STEPHANIE KAMINSKI</h2>
-                <div>Director of Operations</div>
-            </div>
+
+        <div className="flex flex-col items-center font-work-sans text-center lg:w-[30%]">
+          <img
+            src="images/placeholder-vertical.png"
+            className="h-[280px] sm:h-[320px] lg:h-[360px] object-cover mb-4"
+            alt="Mark Fedenia"
+          />
+          <h2 className="font-songer text-lg font-bold">MARK FEDENIA, PH.D</h2>
+          <div>Director of Investments</div>
         </div>
-        <button onClick={() => console.log("View More Team Members")}>
-            VIEW MORE TEAM MEMBERS
-        </button>
+
+        <div className="flex flex-col items-center font-work-sans text-center lg:w-[30%]">
+          <img
+            src="images/placeholder-vertical.png"
+            className="h-[280px] sm:h-[320px] lg:h-[360px] object-cover mb-4"
+            alt="Stephanie Kaminski"
+          ></img>
+          <h2 className="font-songer text-lg font-bold">STEPHANIE KAMINSKI</h2>
+          <div>Director of Operations</div>
+        </div>
+      </div>
+
+      <button
+        onClick={() => console.log("View More Team Members")}
+        className="font-songer text-white bg-bold-blue rounded-full py-1.5 px-8 my-[50px] cursor-pointer "
+      >
+        VIEW MORE TEAM MEMBERS
+      </button>
     </div>
   );
 }
